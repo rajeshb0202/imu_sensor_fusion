b123937eb301581c50312579ff0e4b13e710389b
rajesh <rajeshb0202@gmail.com>
2025-02-01 15:32:18 +0530

    Calibrated the IMU sensor

--- test_bn055.ino
+++ test_bn055.ino
@@ -18,38 +18,27 @@ myIMU.setExtCrystalUse(true);
  
 void loop() {
   // put your main code here, to run repeatedly:
+uint8_t system, gyro, accel, mag = 0;
+myIMU.getCalibration(&system, &gyro, &accel, &mag);
+
 imu::Vector<3> acc =myIMU.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
-imu::Vector<3> gyro =myIMU.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
-imu::Vector<3> mag =myIMU.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER);
- 
-float acc_x = acc.x();
-float acc_y = acc.y();
-float acc_z = acc.z();
 
 
-// Serial.print("Acc_x");
-// Serial.print("Acc_y");
-// Serial.print("Acc_z");
-
-
-Serial.print(acc_x);
+Serial.print(acc.x());
 Serial.print(",");
-Serial.print(acc_y);
+Serial.print(acc.y());
 Serial.print(",");
-Serial.println(acc_z);
- 
-// Serial.print(gyro.x());
-// Serial.print(",");
-// Serial.print(gyro.y());
-// Serial.print(",");
-// Serial.print(gyro.z());
-// Serial.print(",");
- 
-// Serial.print(mag.x());
-// Serial.print(",");
-// Serial.print(mag.y());
-// Serial.print(",");
-// Serial.println(mag.z());
+Serial.print(acc.z());
+
+Serial.print(",");
+Serial.print(accel);
+Serial.print(",");
+Serial.print(gyro);
+Serial.print(",");
+Serial.print(mag);
+Serial.print(",");
+Serial.println(system);
+
  
 delay(BNO055_SAMPLERATE_DELAY_MS);
  
